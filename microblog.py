c01bc9853278c778f609197bdba2461e5cba6fc9
Richard Ooms <oomsrichard@gmail.com>
2019-06-12 17:26:59 +0200

    remove cli

--- microblog.py
+++ microblog.py
@@ -1,11 +1,9 @@
-from app import create_app, db, cli
+from app import create_app, db
 from app.models import User,  User_dataset, Data_subset, Analysis_result, Dataset_columns, Task
 
 app = create_app()
-cli.register(app)
 
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post, 'Message': Message,
-            'Notification': Notification, 'Task': Task}
+    return {'db': db, 'User': User, 'Task': Task}
