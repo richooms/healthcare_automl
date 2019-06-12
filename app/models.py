b6ceb19478306c098c4589841fbf6e1355998139
Richard Ooms <oomsrichard@gmail.com>
2019-06-12 15:32:52 +0200

    edited my

--- app/models.py
+++ app/models.py
@@ -66,14 +66,14 @@ class User(UserMixin, db.Model):
 
     def my_subsets(self):
         subsets = Data_subset.query.join(
-            User_dataset, (Data_subset.dataset_name == User_dataset.dataset)).filter(
+            User_dataset, (Data_subset.dataset_name == str(self.id) + "-" + User_dataset.dataset)).filter(
                 User_dataset.user_id == self.id)
         return subsets.order_by(User_dataset.id.desc())
 
     def my_results(self):
         results = Analysis_result.query.join(
             Data_subset, (Data_subset.subset == Analysis_result.subset_name)).join(
-                User_dataset, (Data_subset.dataset_name == User_dataset.dataset)).filter(
+                User_dataset, (Data_subset.dataset_name == str(self.id) + "-" + User_dataset.dataset)).filter(
                     User_dataset.user_id == self.id)
         return results.order_by(Analysis_result.id.desc())
 
